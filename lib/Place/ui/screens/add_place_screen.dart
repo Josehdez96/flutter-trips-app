f08c0d64a1ffee3cd8d56187a0f4778c001ecb37
josed.hernandez <josed.hernandez@pragma.com.co>
2021-06-16 07:07:06 -0500

    feat(add_place_screen): textInputs added

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:travel_app/Place/ui/widgets/text_input_location.dart';
 import 'package:travel_app/widgets/gradient_back.dart';
+import 'package:travel_app/widgets/text_input.dart';
 import 'package:travel_app/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -17,6 +19,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+    final _controllerLocationPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: [
@@ -51,6 +57,37 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                 )
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: [
+                Container(), //foto
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: 'Title',
+                    inputType: null,
+                    controller: _controllerTitlePlace,
+                    maxLines: 1,
+                  ),
+                ),
+                TextInput(
+                  hintText: 'Description',
+                  inputType: TextInputType.multiline,
+                  controller: _controllerDescriptionPlace,
+                  maxLines: 4,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: TextInputLocation(
+                    hintText: 'Add location',
+                    iconData: Icons.location_on_outlined,
+                    controller: _controllerLocationPlace
+                  ),
+                )
+              ],
+            ),
           )
         ]
       ),
